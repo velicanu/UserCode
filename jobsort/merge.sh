c170c920e1dd2999107509556849d74ad607e2bd
Dragos Velicanu <velicanu@cern.ch>
2013-09-23 11:10:11 -0400

    added working version of merging scripts

--- jobsort/merge.sh
+++ jobsort/merge.sh
@@ -1,5 +1,6 @@
-find $1|grep root|awk '\
-BEGIN{i=0;c=0;print "mkdir -p mergedTmp/0"}{i++}{if (i>100) \
-{print "hadd mergedTmp/"c".root mergedTmp/"c"/*.root";c++; i=0;print "mkdir -p mergedTmp/"c}}\
-{print "ln -s "$1,"mergedTmp/"c"/"}{sub(".*/","",$1)}{print "./check.sh mergedTmp/"c"/"$1"|bash"}END{print "hadd mergedFile.root mergedTmp/*.root";}\
-'
+start=$((($1+1)*$4))
+mkdir mergedTmp
+cat $2 | head -n $start | tail -n $4 | awk -v filename=$1 -v outdir=$3 -v nfiles=$4 '{print "ln -s "$1" mergedTmp/"}' | bash
+# echo | awk -v filename=$1 -v outdir=$3 -v nfiles=$4 '{print "root -b -q \"mergeForest.C+(\\\"mergedTmp/*.root\\\",\\\""filename".root\\\")\""}' | bash
+echo | awk -v filename=$1 -v outdir=$3 -v nfiles=$4 '{print "./mergeForest.exe \"mergedTmp/*.root\" \""filename".root\""}' | bash
+mv $1.root $3
